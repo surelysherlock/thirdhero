66f77ad34600785c5a99bf9ad6b6ea0e40b61b84
Sanyan Sharar <raad34@gmail.com>
2021-07-08 13:42:12 -0400

    added heroku

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/ThirdHero'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname +  '/dist/ThirdHero/index.html'));});
+app.listen(process.env.PORT || 8080);
